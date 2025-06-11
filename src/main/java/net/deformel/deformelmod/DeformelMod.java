2bd67ae4bc7cfea9ae91a20333bd1c54fd44b63e
deformel <vova02329@gmail.com>
2025-06-11 05:22:01 +0500

    Initial commit

---
+++ src/main/java/net/deformel/deformelmod/DeformelMod.java
@@ -0,0 +1,55 @@
+package net.deformel.deformelmod;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+// Значение здесь должно соответствовать записи в файле META-INF/mods.toml
+@Mod(DeformelMod.MOD_ID)
+public class DeformelMod {
+    // Укажите идентификатор мода в общем месте, чтобы на него можно было ссылаться
+    public static final String MOD_ID = "deformelmod";
+    // Прямая ссылка на логгер slf4j
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public DeformelMod(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    // Добавить предмет примера блока во вкладку строительных блоков
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    // Вы можете использовать SubscribeEvent и позволить шине событий обнаруживать методы для вызова
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    // Вы можете использовать EventBusSubscriber для автоматической регистрации всех статических методов в классе, помеченных @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
